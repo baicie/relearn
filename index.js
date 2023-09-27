f28ab8f581ccfa71318138d8ef2d4a78e1be3489
white-cie <2055572879@qq.com>
2023-09-27 23:55:30 +0800

    feat(custom): ownership
    ISSUES CLOSED: g

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-const p = new Promise()
+const arr = [
+  { name: '66', sex: '男' },
+  { name: 'hh', sex: '女' },
+  { name: '99', sex: '男' },
+  { name: '大哥', sex: '男' },
+  { name: '巨型萝莉', sex: '女' }
+]
 
-p.then().catch().finally()
+function main(data, key) {
+  const res = {}
+  for (const item of data) {
+    const inneKey = item[key]
+
+    if (!res[inneKey]) {
+      res[inneKey] = []
+    }
+    res[inneKey].push(item)
+  }
+  return res
+}
+
+const res = main(arr, 'sex')
+
+console.log(res);
