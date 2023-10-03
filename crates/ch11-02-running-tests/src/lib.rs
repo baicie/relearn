c6af95d2b7d55b1998f1cc5396c65fab564394e6
white-cie <2055572879@qq.com>
2023-10-03 15:38:50 +0800

    feat: test

--- crates/ch11-02-running-tests/src/lib.rs
+++ crates/ch11-02-running-tests/src/lib.rs
@@ -1,14 +1,36 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// pub fn add_two(a: i32) -> i32 {
+//     a + 2
+// }
+
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn add_two_and_two() {
+//         assert_eq!(4, add_two(2));
+//     }
+
+//     #[test]
+//     fn add_three_and_two() {
+//         assert_eq!(5, add_two(3));
+//     }
+
+//     #[test]
+//     fn one_hundred() {
+//         assert_eq!(102, add_two(100));
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        assert_eq!(4, 2 + 2);
+    }
+
+    #[test]
+    #[ignore]
+    fn expensive_test() {
+        assert_eq!(5, 1 + 1 + 1 + 1 + 1);
     }
 }
