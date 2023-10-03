3a92ca1d334cb51ada7d5f3d4ecc5b64ef5dc300
white-cie <2055572879@qq.com>
2023-10-03 23:12:17 +0800

    feat(custom): 迭代器

--- crates/ch12-01-accepting-command-line-arguments/src/lib.rs
+++ crates/ch12-01-accepting-command-line-arguments/src/lib.rs
@@ -23,12 +23,19 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("少参数了");
         }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err(""),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err(""),
+        };
         let case_sensitive = env::var("CASE_SENSITIVE").is_err();
         Ok(Config {
             query,
@@ -50,16 +57,21 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 }
 
 pub fn search_case_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    let query = query.to_lowercase();
+    // let mut results = Vec::new();
+    // let query = query.to_lowercase();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
+    // for line in contents.lines() {
+    //     if line.to_lowercase().contains(&query) {
+    //         results.push(line);
+    //     }
+    // }
 
-    results
+    // results
+
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 #[cfg(test)]
