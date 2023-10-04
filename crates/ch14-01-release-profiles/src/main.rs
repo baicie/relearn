aab336439b8576ab5e48c1f32ff5184135b05ca1
white-cie <2055572879@qq.com>
2023-10-04 20:20:59 +0800

    feat(custom): publish

---
+++ crates/ch14-01-release-profiles/src/main.rs
@@ -0,0 +1,8 @@
+use ch14_01_release_profiles::mix;
+use ch14_01_release_profiles::PrimaryColor;
+
+fn main() {
+    let red = PrimaryColor::Red;
+    let yellow = PrimaryColor::Yellow;
+    mix(red, yellow);
+}
