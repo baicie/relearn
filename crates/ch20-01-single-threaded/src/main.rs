4409f89df0613bed94f639b0b2464b60a7fbfd31
white-cie <2055572879@qq.com>
2023-10-07 23:14:52 +0800

    feat(custom): rust完结

--- crates/ch20-01-single-threaded/src/main.rs
+++ crates/ch20-01-single-threaded/src/main.rs
@@ -3,12 +3,11 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
         pool.execute(|| {
