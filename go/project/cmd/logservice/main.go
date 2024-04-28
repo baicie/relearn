6178b11a9a9eb5ed590d68688ff0c1f258e7cd75
刘志伟 <2055572879@qq.com>
2024-04-28 10:02:10 +0800

    7

--- go/project/cmd/logservice/main.go
+++ go/project/cmd/logservice/main.go
@@ -6,17 +6,23 @@ import (
 	stlog "log"
 
 	"distributed/log"
+	"distributed/registry"
 	"distributed/service"
 )
 
 func main() {
 	log.Run("./distributed.log")
 	host, port := "localhost", "5001"
+	serviceAddress := fmt.Sprintf("http://%s:%s", host, port)
+	r := registry.Registration{
+		ServiceName: "Log Service",
+		ServiceURL:  serviceAddress,
+	}
 	ctx, err := service.Start(
 		context.Background(),
-		"Log Service",
 		host,
 		port,
+		r,
 		log.RegisterHandlers,
 	)
 	if err != nil {
--- go/project/service/service.go
+++ go/project/service/service.go
@@ -2,19 +2,24 @@ package service
 
 import (
 	"context"
+	"distributed/registry"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func Start(ctx context.Context, serviceName, host, port string,
+func Start(ctx context.Context, host, port string, reg registry.Registration,
 	registerHandlerFunc func()) (context.Context, error) {
 	registerHandlerFunc()
-	ctx = startService(ctx, serviceName, host, port)
+	ctx = startService(ctx, reg.ServiceName, host, port)
+	err := registry.RegisterServie(reg)
+	if err != nil {
+		return ctx, err
+	}
 	return ctx, nil
 }
 
-func startService(ctx context.Context, serviceName, host, port string) context.Context {
+func startService(ctx context.Context, serviceName registry.ServiceName, host, port string) context.Context {
 	ctx, cancel := context.WithCancel(ctx)
 	var srv http.Server
 	srv.Addr = host + ":" + port
