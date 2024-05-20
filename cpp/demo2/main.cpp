b9deb8843db15ff5348d870b21629689d4831552
刘志伟 <2055572879@qq.com>
2024-05-20 20:26:01 +0800

    cmake

--- cpp/demo2/main.cpp
+++ cpp/demo2/main.cpp
@@ -4,7 +4,39 @@ using namespace std;
 
 int main()
 {
+
     WorkerManager wm;
-    wm.Show_Menu();
+    int choice = 0;
+    while (true)
+    {
+        // 展示菜单
+        wm.Show_Menu();
+        cout << "请输入您的选择:" << endl;
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 0: // 退出系统
+            break;
+        case 1: // 添加职工
+            break;
+        case 2: // 显示职工
+            break;
+        case 3: // 删除职工
+            break;
+        case 4: // 修改职工
+            break;
+        case 5: // 查找职工
+            break;
+        case 6: // 排序职工
+            break;
+        case 7: // 清空文件
+            break;
+        default:
+            system("cls");
+            break;
+        }
+    }
+
     return 0;
 }
